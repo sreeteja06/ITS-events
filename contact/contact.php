e6bbf053c1ae240c273f5febbc93c7546811ae43
Sree Teja <sreeteja.muthyala@gmail.com>
2018-09-09 13:15:11 +0530

    removed sendmail.php and added the functionality into contact itself

--- contact/contact.php
+++ contact/contact.php
@@ -1,3 +1,28 @@
+<?php
+if(isset($_POST['submit'])){
+$email=$_POST['Email'];
+// Sanitize E-mail Address
+$email =filter_var($email, FILTER_SANITIZE_EMAIL);
+// Validate E-mail Address
+$email= filter_var($email, FILTER_VALIDATE_EMAIL);
+if (!$email){
+echo "Invalid Sender's Email";
+}
+else{
+$Phone = $_POST['Phone'];
+$message = $_POST['Message'];
+$headers = 'From:'. $email2 . "rn"; // Sender's Email
+$headers .= 'Cc:'. $email2 . "rn"; // Carbon copy to Sender
+// Message lines should not exceed 70 characters (PHP rule), so wrap it
+$message = $message." \n\nMessage sent by ".$_POST['name']."\nemail id ".$email."\nphone number ".$Phone;
+// Send Mail By PHP Mail Function
+mail("itstechclub@gmail.com", $email, $message, $headers);
+// echo "Your mail has been sent successfuly ! Thank you for your feedback go to <a href = "."../index.html".">home</a>";
+echo "<script type='text/javascript'>alert('Your mail has been sent successfuly ! Thank you for your feedback');</script>";
+}
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -19,7 +44,7 @@
             }
             else {
                 console.log("else");
-                let classn = document.getElementsByClassName("cd-stretchy-nav");
+                let classn = document.getElementsByClassName("cd-stretchy-nav") ;
                 classn[0].classList.remove("nav-is-visible");
                 closed = !closed;
             }
@@ -55,7 +80,7 @@
                     src="linkedin.png" height="64px" width="64px"></a>
             <a href="https://twitter.com/sreeteja06" class="Twitter"><img src="twitter.png" height="64px" width="64px"></a>
         </div>
-        <form action="./sendEmail.php" method="POST">
+        <form action="" name="form" id="form" method="POST">
             <div class="row input-container">
                 <div class="styled-input wide">
                     <input id="name" name="name" type="text" required />
@@ -73,7 +98,7 @@
                     <textarea id="Message" name="Message" required></textarea>
                     <label>Message</label>
                 </div>
-                <input type="submit" class="btn-lrg submit-btn" value="Send Message">
+                <input type="submit" name="submit" class="btn-lrg submit-btn" value="Send Message">
             </div>
         </form>
     </div>
